e270c3f0c01de8047639a889e767d29519b6b337
ganesh229 <73064223+ganesh229@users.noreply.github.com>
2020-10-18 18:42:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+a = input("Enter your first number to add it: ")
+b = input("Enter your second number to add it: ")
+a = int(a)
+b = int(b)
+sum = a+b
+print("Your sum for these two number will be: ", sum)
